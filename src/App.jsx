e41c6a328dae66d427f4c37e103aee5aba491942
vats-tushar <121571005+vats-tushar@users.noreply.github.com>
2025-02-02 21:22:53 +0530

    Routing Fixed and theme selecter 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,13 @@ import MainPage from './components/MainPage';
 import { useState } from 'react';
 
 function App() {
-  const [dark, setDark] = useState(false);
-  function handleTheme() {
-    setDark(prev=>!prev);
-  }
+  
 
   return (
-    <div className={`fullPage ${dark ? 'darkBack' : 'lightBack'}`}>
+    <div className={`fullPage`}>
       <Router>
-        <div className={dark ? ' header-dark' : ' header-light'}>
-          <span style={{fontWeight : '600', fontSize: '28px', fontFamily: 'cursive'}}>Video Converter</span>
-
-          <label htmlFor="theme" className="theme">
-            <span className="theme__toggle-wrap">
-              <input id="theme" className="theme__toggle" type="checkbox" role="switch" name="theme" value="dark" checked={dark} onChange={handleTheme}/>
-              <span className="theme__fill"></span>
-              <span className={dark ? 'theme__icon2' : 'theme__icon'}>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-                <span className="theme__icon-part"></span>
-              </span>
-            </span>
-          </label>
-        </div>
         <Routes>
-          <Route exact path='/' element={<MainPage selectTheme={dark}/>}></Route>
+          <Route exact path='/' element={<MainPage />}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,8 +2,12 @@ import {useState} from 'react';
 import VideoConverter from 'convert-video';
 import style from './MainPage.module.css';
 
-const MainPage = ({selectTheme}) => {
+const MainPage = () => {
   const [count, setCount] = useState([]);
+  const [dark, setDark] = useState(false);
+  function handleTheme() {
+    setDark(prev=>!prev);
+  }
   const [convertedFiles, setConvertedFiles] = useState([]);
   function handleUploadVideo(e){
     setCount(e.target.files);
@@ -25,18 +29,39 @@ const MainPage = ({selectTheme}) => {
     
   }
   return (
-    <>
+    <><div className={`fullPage ${dark ? 'darkBack' : 'lightBack'}`}>
+      <div className={dark ? ' header-dark' : ' header-light'}>
+          <span style={{fontWeight : '600', fontSize: '28px', fontFamily: 'cursive'}}>Video Converter</span>
+
+          <label htmlFor="theme" className="theme">
+            <span className="theme__toggle-wrap">
+              <input id="theme" className="theme__toggle" type="checkbox" role="switch" name="theme" value="dark" checked={dark} onChange={handleTheme}/>
+              <span className="theme__fill"></span>
+              <span className={dark ? 'theme__icon2' : 'theme__icon'}>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+                <span className="theme__icon-part"></span>
+              </span>
+            </span>
+          </label>
+        </div>
         <div className={style.inputBlock}>
-            <input className={selectTheme ? style.dark : style.light} type='file' accept='.mov' onChange={handleUploadVideo} multiple/>
+            <input className={dark ? style.dark : style.light} type='file' accept='.mov' onChange={handleUploadVideo} multiple/>
             <button onClick={handleConvert}>Convert</button>
         </div>
         <div style={{display: 'flex', width : '100vw', justifyContent : 'center', alignItems: 'center'}}>
-          <div style={{marginRight: '6px', color : selectTheme ? 'white' : 'black'}}>
+          <div style={{marginRight: '6px', color : dark ? 'white' : 'black'}}>
               {convertedFiles.length} Converted
           </div>
           <button onClick={downloadVideo}>Download</button>
         </div>
-        
+    </div> 
     </>
   )
 }
